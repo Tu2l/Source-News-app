9a91a3b284935aa583139ebf6f40b18610785fa9
Tutul Deb Roy <debroytutul@gmail.com>
2021-08-22 19:01:44 +0530

    22-08-2021

--- app/src/main/java/com/tu2l/source/MainActivity.kt
+++ app/src/main/java/com/tu2l/source/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.tu2l.source
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
@@ -17,8 +22,8 @@ class MainActivity : AppCompatActivity() {
         val pager = findViewById<ViewPager?>(R.id.view_pager)
         val tabs = findViewById<TabLayout?>(R.id.tab_layout)
 
-        val fragments: MutableList<Fragment> = ArrayList<Fragment>();
-        val names: MutableList<String> = ArrayList<String>();
+        val fragments: MutableList<Fragment> = ArrayList<Fragment>()
+        val names: MutableList<String> = ArrayList<String>()
 
         names.add("World")
         fragments.add(NewsListFragment.newInstance(ANI.WORLD_BASE))
@@ -31,7 +36,23 @@ class MainActivity : AppCompatActivity() {
 
         pager?.adapter = TabsAdapter(supportFragmentManager, names, fragments)
         tabs?.setupWithViewPager(pager)
+
+
+        val toolBar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolBar)
+        supportActionBar?.title = resources.getString(R.string.app_name)
     }
 
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        (menu?.findItem(R.id.search)?.actionView as SearchView).apply {
+            setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        }
+
+        return true
+    }
 }
